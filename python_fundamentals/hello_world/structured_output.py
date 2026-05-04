fb0bb1a8694a970ee23ef4b48f530a2a1ec04d7b
artemis-bunbun <starfruitworld@gmail.com>
2026-05-04 08:40:34 +0000

    fix: syntax fix

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
-import os
-
-for f in os.listdir("/checker"):
-    print(f)
-#x = 1
-#if x != 0:
-#	print("Language: Python")
-#	print("Version: 3")
-#	print("Pi approx: 3.14")
-#	print("Computation valid: True")
+x = 1
+if x != 0:
+	print("Language: Python")
+	print("Version: 3")
+	print("Pi approx: 3.14")
+	print("Computation valid: True")
