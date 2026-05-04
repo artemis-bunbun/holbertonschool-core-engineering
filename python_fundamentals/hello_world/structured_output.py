7466a94006f8814f94a644b5b5378f0d67d729a1
artemis-bunbun <starfruitworld@gmail.com>
2026-05-04 08:35:06 +0000

    fix: corrected the print

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 
-parent = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+parent = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../.."))
 for f in os.listdir(parent):
     print(f)
 #x = 1
