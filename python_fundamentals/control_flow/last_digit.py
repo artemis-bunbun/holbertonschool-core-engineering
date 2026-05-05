ca2706e1c40baddd784aeedc81ed11035a21b208
artemis-bunbun <starfruitworld@gmail.com>
2026-05-05 07:54:05 +0000

    fix: syntax

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -6,5 +6,5 @@ if ld > 5:
     print(f"Last digit of {number} is {ld} and greater than 5")
 if ld < 6:
     print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
-if ld == 0:
+if ld > 0:
     print(f"Last digit of {number} is {ld} and is 0")
