410061cd107445ee46ec4053553ae6add4549411
artemis-bunbun <starfruitworld@gmail.com>
2026-05-05 08:52:02 +0000

    fix: typo

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+alphabt = ""
 for i in range(ord('a'), ord('z') + 1):
     letter = chr(i)
     if letter not in ['e', 'q']:
-        print("{}".format(letter), end="")
+        alphabt += letter
+print("{}".format(alphabt), end="")
