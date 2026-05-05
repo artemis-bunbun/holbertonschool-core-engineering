b6f9e764a2cf490b0ddd45e3db64bef41fad88e2
artemis-bunbun <starfruitworld@gmail.com>
2026-05-05 08:10:29 +0000

    fix: fixed the negative single digit

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10000, 10000)
 
-ld = number%10
+ld = number - (10 * int(number / 10))
+
 if ld > 5:
     print(f"Last digit of {number} is {ld} and greater than 5")
 if number < 0:
