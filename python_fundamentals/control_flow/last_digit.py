a30f16d73dd244ff76defc1ae1af455bfc343d3c
artemis-bunbun <starfruitworld@gmail.com>
2026-05-05 07:52:00 +0000

    feat: creation: is greater last digit

---
+++ python_fundamentals/control_flow/last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+number = __import__('random').randint(-10000, 10000)
+
+ld = number%10
+if ld > 5:
+    print(f"Last digit of {number} is {ld} and greater than 5")
+if ld < 6:
+    print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
+if ld == 0:
+    print(f"Last digit of {number} is {ld} and is 0")
