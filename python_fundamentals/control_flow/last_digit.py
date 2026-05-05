7ab086cecefc10af3b49cffd45b863b1dfc8fc46
artemis-bunbun <starfruitworld@gmail.com>
2026-05-05 08:16:59 +0000

    fix: typo

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -4,7 +4,7 @@ number = __import__('random').randint(-10000, 10000)
 ld = number - (10 * int(number / 10))
 
 if ld > 5:
-    print(f"Last digit of {number} is {ld} and greater than 5")
+    print(f"Last digit of {number} is {ld} and is greater than 5")
 if number < 0:
     if ld < 6:
         if ld != 0:
