fd1a6654c0357a84cd5dcd247fa7dd3793565a98
artemis-bunbun <starfruitworld@gmail.com>
2026-05-05 08:02:14 +0000

    fix: russian roulette syntax

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -6,10 +6,11 @@ if ld > 5:
     print(f"Last digit of {number} is {ld} and greater than 5")
 if number < 0:
     if ld < 6:
-        print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
+        if ld != 0:
+            print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
 if ld == 0:
     print(f"Last digit of {number} is {ld} and is 0")
-
 if number > 0:
     if ld < 6:
-        print(f"Last digit of -{number} is -{ld} and is less than 6 and not 0")
+        if ld != 0:
+            print(f"Last digit of -{number} is -{ld} and is less than 6 and not 0")
