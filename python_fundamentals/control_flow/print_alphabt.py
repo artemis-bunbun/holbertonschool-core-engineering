836d1b3e23811cc2dfad78c63b3fd3d12931d04e
artemis-bunbun <starfruitworld@gmail.com>
2026-05-05 14:32:31 +0200

    fix: typo

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 alphabt = ""
-print("")
 for i in range(ord('a'), ord('z') + 1):
     letter = chr(i)
     if letter not in ['e', 'q']:
         alphabt += letter
 print("{}".format(alphabt), end="")
+print("")
