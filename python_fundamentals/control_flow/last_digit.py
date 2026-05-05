c98aa61169b1182ac10ac492a5f61b5169422fb3
artemis-bunbun <starfruitworld@gmail.com>
2026-05-05 07:59:29 +0000

    fix: overcomplicated syntax

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -4,7 +4,12 @@ number = __import__('random').randint(-10000, 10000)
 ld = number%10
 if ld > 5:
     print(f"Last digit of {number} is {ld} and greater than 5")
-if ld < 6:
-    print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
-if ld > 0:
+if number < 0:
+    if ld < 6:
+        print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
+if ld == 0:
     print(f"Last digit of {number} is {ld} and is 0")
+
+if number > 0:
+    if ld < 6:
+        print(f"Last digit of -{number} is -{ld} and is less than 6 and not 0")
