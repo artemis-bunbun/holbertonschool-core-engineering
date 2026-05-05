d82878f541dc9152282bbf6a2e983c884b610271
artemis-bunbun <starfruitworld@gmail.com>
2026-05-05 08:18:50 +0000

    fix: syntax

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -5,13 +5,8 @@ ld = number - (10 * int(number / 10))
 
 if ld > 5:
     print(f"Last digit of {number} is {ld} and is greater than 5")
-if number < 0:
-    if ld < 6:
-        if ld != 0:
-            print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
 if ld == 0:
     print(f"Last digit of {number} is {ld} and is 0")
-if number > 0:
-    if ld < 6:
-        if ld != 0:
-            print(f"Last digit of -{number} is -{ld} and is less than 6 and not 0")
+if ld < 6:
+    if ld != 0:
+        print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
