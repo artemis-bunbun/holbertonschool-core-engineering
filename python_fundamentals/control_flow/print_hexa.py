1df2d8ecb2309419fe02aa81203cf0e3439d103a
artemis-bunbun <starfruitworld@gmail.com>
2026-05-07 10:04:54 +0200

    fix: typo

--- python_fundamentals/control_flow/print_hexa.py
+++ python_fundamentals/control_flow/print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python3
-numbers = list(range(0, 99))
-for i in numbers:
-    hexa = hex(i)
-    print("{} = {}".format(i, hexa))
+for i in range(99):
+    print("{} = {}".format(i, hex(i)))
