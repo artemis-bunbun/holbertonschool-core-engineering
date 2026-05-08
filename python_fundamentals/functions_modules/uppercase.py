825a733897f3295505252386a62252db3134fcc0
artemis-bunbun <starfruitworld@gmail.com>
2026-05-08 22:56:10 +0200

    fix: syntax

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+
 def uppercase(str):
+    new_str = ""
+
     for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
-            print(chr(ord(c) - 32), end="")
+        if 97 <= ord(c) <= 122:
+            new_str += chr(ord(c) - 32)
         else:
-            print(c, end="")
-    print()
+            new_str += c
+    print("{}".format(new_str))
