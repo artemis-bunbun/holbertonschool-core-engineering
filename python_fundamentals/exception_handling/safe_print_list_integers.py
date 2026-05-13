e5097d507336402681e43d472f97a1a9a4079071
artemis-bunbun <starfruitworld@gmail.com>
2026-05-13 09:53:12 +0200

    fix: syntax

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
 
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+        except (IndexError, ValueError, TypeError):
             continue
 
     print()
