2664496f9e7ad96a9e7644b659cc7b657a097794
artemis-bunbun <starfruitworld@gmail.com>
2026-05-15 22:18:04 +0200

    feat: creation:  2 square 2 furious

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Square class built on Rectangle."""
+
+Rectangle = __import__("2-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square as a specialized rectangle."""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Return a readable square description."""
+        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
