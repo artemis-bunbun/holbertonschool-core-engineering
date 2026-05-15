088483b2752488fbf0069b3fb2445dc582b2faf3
artemis-bunbun <starfruitworld@gmail.com>
2026-05-15 21:58:53 +0200

    feat: creation: 1 rectangle

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Rectangle class built on BaseGeometry."""
+
+BaseGeometry = __import__("base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle defined by width and height."""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
