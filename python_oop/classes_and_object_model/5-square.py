97a94c175c289ce98dd72401d56557df987883bd
artemis-bunbun <starfruitworld@gmail.com>
2026-05-15 20:50:01 +0200

    feat: creation: 5 square

---
+++ python_oop/classes_and_object_model/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+idk filler stuff
+"""
+
+
+class Square:
+    """
+    more filler comments
+    """
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
